a1ee0b25b21b27e438680bfa05c03732a768bda0
Isagi yoichi <mouhammadk44@gmail.com>
2023-07-25 20:56:49 +0300

    finishing side bar

--- Youtuby/js/index.js
+++ Youtuby/js/index.js
@@ -1,6 +1,23 @@
- $(document).ready(function () {
+$(document).ready(function () {
     $(".nav-item").click(function () {
         $(".nav-item").removeClass("active");
         $(this).addClass("active");
+    });
+
+    $("#toggler").click(function () {
+        $("#wrap").toggleClass('toggled');
+        var right = $('.sidebar').css("right");
+        if (right == '0px') {
+            $('.sidebar').css({ 'right': '-17rem' });
+            $('.layer').fadeOut();
+
+        }else{
+            $('.sidebar').css({'right':'0'});
+            $('.layer').fadeIn();
+        }
+    });
+    $('.layer').click(function(){
+        $('.sidebar').css({ 'right': '-17rem' });
+        $('.layer').fadeOut();
     });
 });
