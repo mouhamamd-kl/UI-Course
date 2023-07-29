bc9f4a02ed9cf2b61145d0e8eaffea6466e92e0c
mouhammadkl <mouhammadk44@gmail.com>
2023-07-29 15:57:49 +0300

    .

--- Youtuby/js/watch.js
+++ Youtuby/js/watch.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
     $('#see-more').click(function() {
         $("#video-description").toggleClass("wrapped");
     });
-    
+    $(".reply").click(function(){
+        $()
+    });
   });
