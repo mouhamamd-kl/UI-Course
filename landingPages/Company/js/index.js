e967fa807a126b8a3fa101aa98b0b327d80de59f
mouhammadkl <mouhammadk44@gmail.com>
2023-08-18 21:17:16 +0300

    styling statistics section with dynamic animation

--- landingPages/Company/js/index.js
+++ landingPages/Company/js/index.js
@@ -8,3 +8,14 @@ function scrollFunction(){
 window.onscroll=function(){
     scrollFunction();
 }
+
+window.onload=function(){
+    let website=new CountUp('website-count',0,250,0,2.5);
+    let app=new CountUp('app-count',0,100,0,2.5);
+    let user=new CountUp('user-count',0,10000,0,2.5);
+    let video=new CountUp('video-count',0,30,0,2.5);
+    website.start();
+    app.start();
+    user.start();
+    video.start();
+}
