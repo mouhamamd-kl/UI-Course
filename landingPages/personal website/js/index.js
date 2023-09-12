c86d4f24ede494a9eb7c5dcf1908b21129c7f165
mouhammadkl <mouhammadk44@gmail.com>
2023-09-12 21:28:22 +0300

    styling statistics section

--- landingPages/personal website/js/index.js
+++ landingPages/personal website/js/index.js
@@ -5,3 +5,11 @@ $(window).scroll(function () {
     $("#navbar").removeClass("noTransparent");
   }
 });
+$(document).ready(function(){
+  $('.circle').circleProgress({
+    startAngle:-Math.PI/2,
+    fill:"#0575e6"
+  }).on('circle-animation-progress',function(event,progress,stepValue){
+    $(this).find('span').html(Math.round(stepValue*100)+'%')
+  })
+})
