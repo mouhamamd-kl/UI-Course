5e12a6f1062fcd1b5f83234d68e52466ab9ea982
mouhammadkl <mouhammadk44@gmail.com>
2023-09-18 23:00:04 +0300

    adding validation for contact me form

--- landingPages/personal website/js/index.js
+++ landingPages/personal website/js/index.js
@@ -11,5 +11,14 @@ $(document).ready(function(){
     fill:"#0575e6"
   }).on('circle-animation-progress',function(event,progress,stepValue){
     $(this).find('span').html(Math.round(stepValue*100)+'%')
+  });
+
+  $(function(){
+    $('#contactForm').validate();
+  });
+  $(function(){
+    if ($('section.contacts .contact-info .contact-form label').is(':visible')) {
+      $('section.contacts .contact-info .contact-form input, section.contacts .contact-info .contact-form textarea').css("margin-bottom", "0");
+  }
   })
 })
