cd77d5c9679997f22daed60f9524a5b8cce9c850
mouhammadkl <mouhammadk44@gmail.com>
2023-09-27 19:24:51 +0300

    adding html-loader

--- onlineStore/webpack.config.js
+++ onlineStore/webpack.config.js
@@ -1,10 +1,28 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/.indexjs',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options:{
+          minimize:true,
+        }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
 };
