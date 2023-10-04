0f5e4dfc234d34e14ce7d4fa527a1e8b662e672f
Isagi yoichi <mouhammadk44@gmail.com>
2023-10-04 13:37:27 +0300

    adding tooltip to the shopping cart icon

--- onlineStore/src/index.js
+++ onlineStore/src/index.js
@@ -2,5 +2,7 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/style.css'
 import '@fortawesome/fontawesome-free/js/all.min.js'
-console.log("hello")
-console.log("hello2")
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
