8664a103dffc0ce096755c0fed69a2459fd94007
mouhammadkl <mouhammadk44@gmail.com>
2023-10-13 19:34:40 +0300

    final touches to most sold products section

--- onlineStore/src/index.js
+++ onlineStore/src/index.js
@@ -6,3 +6,9 @@ window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item=>{
+    item.addEventListener("click",()=>{
+        alert("أضيف المنتج إلى عربة الشراء .")
+    })
+})
