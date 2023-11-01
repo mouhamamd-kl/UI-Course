c423ae06bf53269119c04a0453485013f9989ca7
mouhammadkl <mouhammadk44@gmail.com>
2023-11-01 22:38:28 +0300

    styling product specifications

--- onlineStore/src/index.js
+++ onlineStore/src/index.js
@@ -1,18 +1,43 @@
-import '@fortawesome/fontawesome-free/js/all.min.js'
+import "@fortawesome/fontawesome-free/js/all.min.js";
 // import 'bootstrap/dist/js/bootstrap.min.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/style.css'
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+document.querySelectorAll(".product-description").forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("hefdghdfgllo");
+  });
+});
 
-document.querySelectorAll('.add-to-cart-btn').forEach(item=>{
-    item.addEventListener("click",()=>{
-        alert("أضيف المنتج إلى عربة الشراء .")
-    })
-})
+document.querySelectorAll('.product-option input[name="color"]').forEach((item) => {
+  item.addEventListener("change", () => {
+    document.querySelectorAll(".product-option.color-option").forEach((i) => {
+      i.classList.remove("active");
+    });
+    item.parentNode.parentNode.classList.add("active");
+  });
+});
+document.querySelectorAll('.product-option input[name="size"]').forEach((item) => {
+    item.addEventListener("change", () => {
+      document.querySelectorAll(".product-option.size-option").forEach((i) => {
+        i.classList.remove("active");
+      });
+      item.parentNode.parentNode.classList.add("active");
+    });
+  });
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+document.querySelectorAll(".add-to-cart-btn").forEach((item) => {
+  item.addEventListener("click", () => {
+    alert("أضيف المنتج إلى عربة الشراء .");
+  });
+});
 var d = new Date();
 var year = d.getFullYear();
-document.getElementById("year").innerHTML = year+"&copy;";
-
+document.getElementById("year").innerHTML = year + "&copy;";
