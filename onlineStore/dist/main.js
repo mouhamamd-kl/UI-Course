a38b337914750ca864c110cfe341c8f7e695250e
mouhammadkl <mouhammadk44@gmail.com>
2023-11-24 19:56:59 +0300

    finishing contact page

--- onlineStore/dist/main.js
+++ onlineStore/dist/main.js
@@ -340,7 +340,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _for
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("aae7c688c5050eba0381")
+/******/ 		__webpack_require__.h = () => ("df78cd91941ac393375b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
